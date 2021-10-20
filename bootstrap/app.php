2d0152121ca28bf5bf7fb0fb30d830de03436b25
Samson Adejoro <samson.adejoro@concept-nova.com>
2021-10-20 14:58:01 +0100

    Added mongodb library

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,10 +22,11 @@ date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
+$app->register(Jenssegers\Mongodb\MongodbServiceProvider::class);
 
-// $app->withFacades();
+$app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
